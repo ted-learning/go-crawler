2451d003ab6f48013d2290a41082a69671dafb98
Zhang Gang <gzhang@thoughtworks.com>
2022-03-07 18:51:07 +0800

    add timer

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"go-crawler/engine"
 	"go-crawler/parser"
+	"log"
+	"time"
 )
 
 func main() {
+	s := time.Now()
 	engine.Run(engine.Request{
 		Url:        "https://nba.stats.qq.com/team/list.htm",
 		ParserFunc: parser.ParseIndex,
 	})
+	e := time.Now()
+	log.Printf("Total crawler cost: %f", e.Sub(s).Seconds())
 }
